f13e0b84992dc4cc9c2547321e8759c8e0485b83
David-Dorito <fercone75@gmail.com>
2026-02-17 22:45:44 +0100

    implemented GfxLib header file and created source file

---
+++ Libs/GfxLib/GfxLib.c
@@ -0,0 +1,35 @@
+#include "GfxLib.h"
+
+typedef int64_t                             i64;
+typedef int32_t                             i32;
+typedef int16_t                             i16;
+typedef int8_t                              i8;
+typedef uint64_t                            u64;
+typedef uint32_t                            u32;
+typedef uint16_t                            u16;
+typedef uint8_t                             u8;
+
+void GfxLib_DrawChar(GfxLib_Handle* pGfxLibHandle, GfxLib_FontChar* pChar, uint16_t posX, uint16_t posY, uint32_t color)
+{
+
+}
+
+void GfxLib_DrawText(GfxLib_Handle* pGfxLibHandle)
+{
+
+}
+
+void GfxLib_DrawLine(GfxLib_Handle* pGfxLibHandle)
+{
+
+}
+
+void GfxLib_DrawRect(GfxLib_Handle* pGfxLibHandle)
+{
+
+}
+
+void GfxLib_DrawCirc(GfxLib_Handle* pGfxLibHandle)
+{
+
+}
---
+++ Libs/GfxLib/GfxLib.h
@@ -0,0 +1,30 @@
+#ifndef GFXLIB_H_
+#define GFXLIB_H_
+
+#include <stdint.h>
+
+typedef struct {
+    uint16_t Width;
+    uint16_t Height;
+    uint8_t* pBitMap;
+} GfxLib_FontChar;
+
+typedef struct {
+    uint16_t CharCount;
+    GfxLib_FontChar* pFontChars;
+} GfxLib_Font;
+
+typedef struct {
+    uint16_t FontCount;
+    GfxLib_Font* pFonts;
+    void* displayHandle;
+    void (*DrawPixelFunc)(void* pHandle, uint16_t posX, uint16_t posY, uint32_t color);
+} GfxLib_Handle; 
+
+void GfxLib_DrawChar(GfxLib_Handle* pGfxLibHandle, GfxLib_FontChar* pChar, uint16_t posX, uin16_t posY, uint32_t color);
+void GfxLib_DrawText(GfxLib_Handle* pGfxLibHandle);
+void GfxLib_DrawLine(GfxLib_Handle* pGfxLibHandle);
+void GfxLib_DrawRect(GfxLib_Handle* pGfxLibHandle);
+void GfxLib_DrawCirc(GfxLib_Handle* pGfxLibHandle);
+
+#endif
