7589cff5a60c2189c03db44fdf035875f64a912a
David-Dorito <fercone75@gmail.com>
2026-02-17 22:46:32 +0100

    implemented DrawChar() in GfxLib src file

--- Libs/GfxLib/GfxLib.c
+++ Libs/GfxLib/GfxLib.c
@@ -11,7 +11,18 @@ typedef uint8_t                             u8;
 
 void GfxLib_DrawChar(GfxLib_Handle* pGfxLibHandle, GfxLib_FontChar* pChar, uint16_t posX, uint16_t posY, uint32_t color)
 {
-
+    u16 row = 0;
+    u16 col = 0;
+    for (u32 i = 0; i < pChar->Height*pChar->Width; i++)
+    {
+        col = i % pChar->Width;
+        row = i / pChar->Width;
+        u32 pixelIndex = col+row*pChar->Width;
+        u8 bitOffset = 7 - (pixelIndex % 8);
+        u8 currentPixelActive = (pChar->pBitMap[pixelIndex/8] >> bitOffset) & 1;
+        if (currentPixelActive)
+            pGfxLibHandle->DrawPixelFunc(pGfxLibHandle->displayHandle, posX+col, posY+row, color);
+    }
 }
 
 void GfxLib_DrawText(GfxLib_Handle* pGfxLibHandle)
