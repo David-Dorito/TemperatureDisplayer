5c8cd3d45d76b41f068c87860c50441a45f31876
David-Dorito <fercone75@gmail.com>
2026-02-22 11:03:18 +0100

    moved bit definition macros into driver src files from header files

--- Drivers/Inc/stm32f401xx_spi_driver.h
+++ Drivers/Inc/stm32f401xx_spi_driver.h
@@ -10,45 +10,6 @@
 
 #include "stm32f401xx.h"
 
-/******************************** BIT DEFINITION MACROS ********************************/
-
-#define SPI_CR1_BIDIMODE            15
-#define SPI_CR1_BIDIOE              14
-#define SPI_CR1_CRCEN               13
-#define SPI_CR1_CRCNEXT             12
-#define SPI_CR1_DFF                 11
-#define SPI_CR1_RXONLY              10
-#define SPI_CR1_SSM                 9
-#define SPI_CR1_SSI                 8
-#define SPI_CR1_LSBFIRST            7
-#define SPI_CR1_SPE                 6
-#define SPI_CR1_BR                  3
-#define SPI_CR1_MSTR                2
-#define SPI_CR1_CPOL                1
-#define SPI_CR1_CPHA                0
-
-#define SPI_CR2_TXEIE               7
-#define SPI_CR2_RXNEIE              6
-#define SPI_CR2_ERRIE               5
-#define SPI_CR2_FRF                 4
-#define SPI_CR2_SSOE                2
-#define SPI_CR2_TXDMAEN             1
-#define SPI_CR2_RXDMAEN             0
-
-#define SPI_SR_FRE                  8
-#define SPI_SR_BSY                  7
-#define SPI_SR_OVR                  6
-#define SPI_SR_MODF                 5
-#define SPI_SR_CRCERR               4
-#define SPI_SR_UDR                  3
-#define SPI_SR_CHSIDE               2
-#define SPI_SR_TXE                  1
-#define SPI_SR_RXNE                 0
-
-#define SPI_I2SCFGR_I2SMOD          11
-
-/***************************************************************************************/
-
 //DeviceMode
 #define SPI_DEVICEMODE_SLAVE        0
 #define SPI_DEVICEMODE_MASTER       1
@@ -91,7 +52,6 @@
 #define SPI2_REG_RESET() do {RCC->APB1RSTR |= (1 << 14); RCC->APB1RSTR &= ~(1 << 14);} while(0)
 #define SPI3_REG_RESET() do {RCC->APB1RSTR |= (1 << 15); RCC->APB1RSTR &= ~(1 << 15);} while(0)
 #define SPI4_REG_RESET() do {RCC->APB2RSTR |= (1 << 13); RCC->APB2RSTR &= ~(1 << 13);} while(0)
-
 
 typedef struct {
     u8 DeviceMode;
